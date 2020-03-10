ae817c88f512e6c8ff07c686149988c99848c558
Daniel Borup <borup.daniel@mayo.edu>
2020-03-10 09:48:34 -0500

    Fix wgetting and untarring

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -7,6 +7,6 @@ DISPLAY=localhost:1.0 xvfb-run -a bash -c "gpi --help"
 # Test gpi_make 
 mkdir gpi
 cd gpi
-wget https://github.com/gpilab/core-nodes/releases/latest
+wget -O - https://github.com/gpilab/core-nodes/tarball/master | tar xz --strip-components 1
 cd gridding
 gpi_make --all --ignore-gpirc --ignore-system-libs
